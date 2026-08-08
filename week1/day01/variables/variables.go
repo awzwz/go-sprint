62a5a938e7538af90d2e9a57e0843adfe2882567
awzwz <awzwz0401@gmail.com>
2026-08-09 04:10:07 +0500

    День 1: переменные и типы

---
+++ week1/day01/variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// здесь твои переменные
+	name := "Aziz"
+	age := 20
+	var height float64 = 1.80
+	isStudent := true
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(height)
+	fmt.Println(isStudent)
+
+}
